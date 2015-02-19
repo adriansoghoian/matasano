174e79b81b9d10754540df026caa4ff8b53e5ae2
adriansoghoian <adriansoghoian@gmail.com>
2015-02-19 00:12:52 -0500

    problem 5 done

--- set1.py
+++ set1.py
@@ -54,42 +54,60 @@ def problem_4(file_path):
     return single_char_xor(candidate[0], candidate[1])
 
 
-def repeating_xor(plaintext, xor_keys):
+def repeating_xor(ptxt, xor_keys):
     """
     Repeating key xor with a single character.
     """
-    pass
+    ctxt = ""
+
+    for i, letter in enumerate(ptxt):
+        key = xor_keys[i % len(xor_keys)]
+        ctxt += chr(ord(letter) ^ ord(key)).encode('hex')
+
+    return ctxt
 
 
+def hamming_distance(string1, string2):
+    return sum(1 for i in range(len(string1)) if string1[i] != string2[i])
+
+
+def problem_6(ctxt_path):
+    ctxt = open(ctxt_path).read().decode('base64')
+    print ctxt
 
 
 if __name__ == "__main__":
-    ### Problem 1
-    input_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-    output_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    print hex_to_base64(input_str) == output_str
-    print "*"*25
+    # ### Problem 1
+    # input_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    # output_str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    # print hex_to_base64(input_str) == output_str
+    # print "*"*25
 
-    ### Problem 2
-    input_str1 = "1c0111001f010100061a024b53535009181c"
-    input_str2 = "686974207468652062756c6c277320657965"
-    output_str = "746865206b696420646f6e277420706c6179"
-    print fixed_xor(input_str1, input_str2)
-    print "*"*25
+    # ### Problem 2
+    # input_str1 = "1c0111001f010100061a024b53535009181c"
+    # input_str2 = "686974207468652062756c6c277320657965"
+    # output_str = "746865206b696420646f6e277420706c6179"
+    # print fixed_xor(input_str1, input_str2)
+    # print "*"*25
 
-    ## Problem 3
-    input_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-    freq_char = char_frequency(input_str)[0]
-    print single_char_xor(input_str, freq_char)
-    print "*"*25
+    # ## Problem 3
+    # input_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+    # freq_char = char_frequency(input_str)[0]
+    # print single_char_xor(input_str, freq_char)
+    # print "*"*25
 
-    ## Problem 4
-    print problem_4("text/set1_problem4.txt")
+    # ## Problem 4
+    # print problem_4("text/set1_problem4.txt")
 
-    ## Problem 5
-    ptxt = "Burning 'em, if you ain't quick and nimble \nI go crazy when I hear a cymbal"
-    desired_ctxt = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    # ## Problem 5
+    # ptxt = open("text/set1_problem5.txt").read()
+    # keys = "ICE"
+    # desired_ctxt = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    # print desired_ctxt == repeating_xor(ptxt, keys)
 
+    ## Problem 6
+    # problem_6("text/set1_problem6.txt")
+    print hamming_distance("this is a test", "wokka wokka!!!")
 
 
 
